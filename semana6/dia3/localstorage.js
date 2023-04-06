4844c1f8fa20f5dda0cc5efabfe917d6fd4b52bf
guillermosifu <sifuentes1201@gmail.com>
2023-04-05 20:37:28 -0500

    local

--- semana6/dia3/localstorage.js
+++ semana6/dia3/localstorage.js
@@ -12,7 +12,50 @@ btnLogin.onclick = function(event){
         //setItem  guardamos el valor en localstorage
      localStorage.setItem(input.name,input.value)
     })
+  
+    login()
+
+}
+
+//como capturo los datos del localStorage 
+//localSotorage.getItem(key)
 
 
 
+
+//como sinsertamos estos datos capturados desde local storage a mi navegador???
+
+
+function login(){
+    const email= localStorage.getItem("email");
+    const password= localStorage.getItem("password");
+    form.style.display= "none";
+    usuarioSection.style.display="block";
+    titleEmail.innerHTML = email;
+    titlePassword.innerHTML = password
+}
+
+
+const form = document.querySelector("#section-login");
+const usuarioSection = document.querySelector("#usuario-logged");
+const titleEmail = document.querySelector("#email");
+const titlePassword = document.querySelector("#password");
+
+
+const btnCerrarSesion = document.querySelector("#cerrar-sesion")
+
+if(email !== null && password !== null){
+    login()
+}else{
+    usuarioSection.style.display="none"
+}
+
+
+btnCerrarSesion.onclick = function(){
+    //vamos eleiminar los dato del local 
+    localStorage.removeItem("password");
+    localStorage.removeItem("email");
+    //mostrar el formulaio y ocultar el mensaje del usuario 
+    form.style.display="block";
+    usuarioSection.style.display="none";
 }
