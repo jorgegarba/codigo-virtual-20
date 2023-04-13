d79d8b8d88291bbffa18a98766af67752071745f
Jorge Garnica <jorgegarba@gmail.com>
2023-04-12 23:15:13 -0500

    ejercicio crear producto

---
+++ semana7/dia3/02-creador-de-productos.js
@@ -0,0 +1,25 @@
+const nombre = document.getElementById('nombre');
+const imagen = document.getElementById('imagen');
+const precio = document.getElementById('precio');
+const stock = document.getElementById('stock');
+const botonCrear = document.getElementById('botonCrear');
+
+const crearProducto = async (producto) => {
+	const URL = 'https://64376df80c58d3b145712b2c.mockapi.io/producto';
+	const response = await fetch(URL, {
+		method: 'POST',
+		body: JSON.stringify(producto)
+	});
+	const data = await response.json();
+	console.log('data', data);
+};
+
+botonCrear.onclick = function () {
+	const objetoProducto = {
+		nombre: nombre.value,
+		stock: +stock.value,
+		precio: +precio.value,
+		img: imagen.value
+	};
+	crearProducto(objetoProducto);
+};
