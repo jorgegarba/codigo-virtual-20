b1af5974097a4148229e36f2e5ffe51512e61f1b
guillermosifu <sifuentes1201@gmail.com>
2023-04-13 21:17:47 -0500

    new

--- semana7/dia4/api.js
+++ semana7/dia4/api.js
@@ -20,6 +20,49 @@
 
 //la estructura de una funciones fecth()
 
+//priemro atrapamos todos los elemtnos que usaremos con el id 
+
+const imageProfile = document.querySelector("#img-profile")
+const githubName = document.querySelector("#github-name")
+const githubUsername = document.querySelector("#github-username")
+const githubJoined = document.querySelector("#github-joined")
+const githubRepos = document.querySelector("#github-repos")
+const githubFollowers = document.querySelector("#github-followers")
+const githubFollowing = document.querySelector("#github-following")
+
+
+//action
+
+const githubActionSearch = document.querySelector("#github-action-search")
+const githubInputSearch = document.querySelector("#github-search")
+
+
+githubActionSearch.onclick=()=>{
+    const username = githubInputSearch.value;
+
+    githubInputSearch.value="";
+    if(username === ""){
+        Swal.fire({
+            title:"Error",
+            text:"Debes llenar el usuario",
+            icon:"error"
+        });
+        return;
+    }
+    obtenerDatosGithub()
+}
+
+
+
+
+
+
+
+
+
+
+
+
 const obtenerDatosGithub = async () => {
   // ene ste ejemplio await esta haciendo lo sgte
 
