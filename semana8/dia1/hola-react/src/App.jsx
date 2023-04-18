f9f66169673b40b66c936f21ed8504dbfdfcbae7
Jorge Garnica <jorgegarba@gmail.com>
2023-04-17 23:04:51 -0500

    primer proyecto en react

---
+++ semana8/dia1/hola-react/src/App.jsx
@@ -0,0 +1,15 @@
+import Cabecera from './Cabecera';
+
+function App() {
+	return (
+		<div>
+			<Cabecera />
+			<Cabecera />
+			<Cabecera />
+			<Cabecera />
+			<h1>Hola mundo</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ semana8/dia1/hola-react/src/Cabecera.jsx
@@ -0,0 +1,10 @@
+function Cabecera() {
+	return (
+		<header>
+			<a href="">Inicio</a>
+			<a href="">Nosotros</a>
+		</header>
+	);
+}
+
+export default Cabecera;
