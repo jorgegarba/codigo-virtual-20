cc39addc18ec8e58ea8a0d4b4afe2a49452d18bd
Jorge Garnica <jorgegarba@gmail.com>
2023-04-18 22:45:22 -0500

    new react proyect

---
+++ semana8/dia2/02-react-products/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  /**
+   * 1. Instalar bootstrap
+   * 2. Crear componenete para mostrar el header de bootstrap, usar el mas sencillo
+   * 3. Crear componenet llamado Productos que mostrará una lista de productos importados 
+   * desde productos.js
+   * 4. Crear un componenete llamado Prodcto que reciba un ojbeto producto
+   * y a su vez, éste componenete debería ser invocado desde el archivo Productos.
+   * 5. Para representar un prodycto, considere usar los cards de bootstrap.
+   * 6. Considerar usar rows, cols, containers, etc.
+   */
+	return <div>Hola mundo</div>;
+}
+
+export default App;
