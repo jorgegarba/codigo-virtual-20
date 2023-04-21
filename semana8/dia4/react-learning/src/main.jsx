e943b60e0951d91b187d179882f4459d94fc035a
guillermosifu <sifuentes1201@gmail.com>
2023-04-20 21:17:36 -0500

    react

---
+++ semana8/dia4/react-learning/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AppEjemplo from './pages/AppEjemplo.jsx'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <AppEjemplo/>
+  </React.StrictMode>,
+)
